d0c6f83c84282ec54dd24284b5e1ad978b69e970
Vlada <vlada-ternova@mail.ru>
2023-06-11 17:43:36 +0300

    задача 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write($"Введите число a:");
+int a = int.Parse (Console.ReadLine());
+Console.Write($"Введите число b:");
+int b = int.Parse (Console.ReadLine());
+Console.Write($"Введите число c:");
+int c = int.Parse (Console.ReadLine());
+int max = a;
+if (b>max) max=b;
+if (c>max) max=c;
+Console.Write("max=");
+Console.WriteLine(max);
---
+++ Задача 4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write($"Введите число a:");
+int a = int.Parse (Console.ReadLine());
+Console.Write($"Введите число b:");
+int b = int.Parse (Console.ReadLine());
+Console.Write($"Введите число c:");
+int c = int.Parse (Console.ReadLine());
+if (a%2==0)
+{Console.WriteLine($"Четное число a = {a}");}
+if (b%2==0)
+{Console.WriteLine($"Четное число b = {a}");}
+if (c%2==0)
+{Console.WriteLine($"Четное число c = {a}");}
+
